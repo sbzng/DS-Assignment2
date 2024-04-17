a88407baa267f2fb6d4298f1eb9528e167dd3207
Shaobo Zang <20105527@mail.wit.ie>
2024-04-17 01:27:47 +0100

    Completed Fan out implementation - added emailing logic

---
+++ env.ts
@@ -0,0 +1,3 @@
+export const SES_REGION = 'eu-west-1';
+export const SES_EMAIL_FROM = 'denggg4399@gmail.com' ;
+export const SES_EMAIL_TO =  'denggg4399@gmail.com';
--- lib/eda-app-stack.ts
+++ lib/eda-app-stack.ts
@@ -27,6 +27,10 @@ export class EDAAppStack extends cdk.Stack {
       receiveMessageWaitTime: cdk.Duration.seconds(10),
     });
 
+    const mailerQ = new sqs.Queue(this, "mailer-queue", {
+      receiveMessageWaitTime: cdk.Duration.seconds(10),
+    });
+
     const newImageTopic = new sns.Topic(this, "NewImageTopic", {
       displayName: "New Image topic",
     });
@@ -43,11 +47,36 @@ export class EDAAppStack extends cdk.Stack {
       }
     );
 
+    const mailerFn = new lambdanode.NodejsFunction(this, "mailer-function", {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      memorySize: 1024,
+      timeout: cdk.Duration.seconds(3),
+      entry: `${__dirname}/../lambdas/mailer.ts`,
+    });
+
+    mailerFn.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "ses:SendEmail",
+          "ses:SendRawEmail",
+          "ses:SendTemplatedEmail",
+        ],
+        resources: ["*"],
+      })
+    );
+
     // S3 --> SQS
     imagesBucket.addEventNotification(
       s3.EventType.OBJECT_CREATED,
       new s3n.SnsDestination(newImageTopic)  // Changed
     );
+
+    newImageTopic.addSubscription(
+      new subs.SqsSubscription(imageProcessQueue)
+    );
+
+    newImageTopic.addSubscription(new subs.SqsSubscription(mailerQ));
 
     // SQS --> Lambda
     const newImageEventSource = new events.SqsEventSource(imageProcessQueue, {
@@ -57,6 +86,12 @@ export class EDAAppStack extends cdk.Stack {
 
     processImageFn.addEventSource(newImageEventSource);
 
+    const newImageMailEventSource = new events.SqsEventSource(mailerQ, {
+      batchSize: 5,
+      maxBatchingWindow: cdk.Duration.seconds(10),
+    }); 
+
+    mailerFn.addEventSource(newImageMailEventSource);
     // Permissions
 
     imagesBucket.grantRead(processImageFn);
@@ -64,9 +99,7 @@ export class EDAAppStack extends cdk.Stack {
 
     // Output
 
-    newImageTopic.addSubscription(
-      new subs.SqsSubscription(imageProcessQueue)
-    );
+
     
     new cdk.CfnOutput(this, "bucketName", {
       value: imagesBucket.bucketName,
